672c0f7a641c329806b590ec2a43ce802b2f4ed5
vinaypathrikarreactjstrainer <tinu.vinau2128@gmail.com>
2025-11-13 19:43:43 +0530

    added varables & introduction of javascript files

--- lectures/introduction/index.js
+++ lectures/introduction/index.js
@@ -1,1 +1,7 @@
 console.log('Javascript : Introduction');
+console.log('Anjali Riyal');
+console.log('Priyanka Kashyap');
+console.log('Muskan Shaikh');
+console.log('Anushka Gupta');
+console.log('Shweta Riyal');
+console.log('Dimpal Patil');
---
+++ lectures/variables/variable.js
@@ -0,0 +1,17 @@
+console.log('Javascript - variables');
+// single line comments
+/*
+  multi line comments
+*/
+
+// VARIABLE : It is a container which stored or hold any data(value)
+/*
+   Variable Declaration: A variable which is defined but does not assigned any value
+   Varibale Initilization : A variable which is defined and also having a value
+*/
+
+// syntax : var varable_name = 'fusion'
+
+var name; // declaration
+name = "Fusion";
+console.log('My Institute name is ', name);
