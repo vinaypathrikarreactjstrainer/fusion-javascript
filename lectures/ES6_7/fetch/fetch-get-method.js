a2021cd0f020b51f4d49cc8aba5e736e94d477b6
vinaypathrikarreactjstrainer <tinu.vinau2128@gmail.com>
2025-12-25 14:41:04 +0530

    resolve the duplicate array of object issue - update the code

--- lectures/ES6_7/fetch/fetch-get-method.js
+++ lectures/ES6_7/fetch/fetch-get-method.js
@@ -6,18 +6,17 @@ let section = document.querySelector('#users'),
 btnList = document.querySelector('#btn-list');
 btnGrid = document.querySelector('#btn-grid');
 
-const getUsers = () => {
-    // debugger;
-    // fetch - GET
-    fetch('https://jsonplaceholder.typicode.com/users')
+// on page load we fetch the data at once
+fetch('https://jsonplaceholder.typicode.com/users')
         .then(resp => resp.json())
         .then(data => showUser(data))
         .catch(err => console.error(err))
-}
 
 // showUser data function
 const showUser = list => {
-    
+    list.sort((a,b) => {return a.id < b.id});
+    console.log('list => ', list);
+
     for(let x = 0; x < list.length; x++){
         div += `<div class="card" id="grid">
                     <h1>${list[x].name}</h1>
@@ -30,7 +29,6 @@ const showUser = list => {
     section.innerHTML = div;
 }
 
-getUsers();
 
 const listView = () => {
     let card = document.querySelectorAll('.card');
@@ -38,8 +36,15 @@ const listView = () => {
     for(let x = 0; x < card.length; x++){
         card[x].setAttribute('id', 'list');
     }
-    // getUsers();
 }
 
+const gridView = () => {
+    let grid = document.querySelectorAll('.card');
+    console.log('grid => ', grid);
+    for(let x = 0; x < grid.length; x++){
+        grid[x].setAttribute('id', 'grid');
+    }
+}
+// Events
 btnList.addEventListener('click', listView);
-btnGrid.addEventListener('click', getUsers);
+btnGrid.addEventListener('click', gridView);
